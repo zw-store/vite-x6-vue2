e5bd627b143e3a50fb91c925101103faeeb5e512
心灵骇客 <1287813343@qq.com>
2024-09-10 15:31:40 +0800

    test: 关闭 edge 选中的边框

--- src/graph/plugin/modules/selection.js
+++ src/graph/plugin/modules/selection.js
@@ -18,7 +18,7 @@ export default graph => {
       // content: '', // 设置附加显示的内容
       // filter: '', // 节点过滤器
       showNodeSelectionBox: true, // 是否显示节点的选择框
-      showEdgeSelectionBox: true, // 是否显示边的选择框
+      showEdgeSelectionBox: false, // 是否显示边的选择框
       pointerEvents: 'auto', // 如果打开 showNodeSelectionBox 时，会在节点上方盖一层元素，导致节点的事件无法响应，此时可以配置 pointerEvents: none 来解决，默认值是 auto
       eventTypes: ['leftMouseDown', 'mouseWheelDown'], // 用于设置框选的触发事件类型
     }),
